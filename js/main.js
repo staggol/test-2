870bbc631cf8d431cb8e80894ef3cbbe3f4347b7
Staggol <gameslegendblood@gmail.com>
2018-07-28 18:35:14 +0400

    almooostt ready

--- js/main.js
+++ js/main.js
@@ -1,24 +1,66 @@
-var app = new Vue ({
-	el: '#app',
-	data: {
-		newElement: '',
+Vue.use(Vuex)
+
+const store = new Vuex.Store({
+    state: {
+    	newElement: '',
 		idForElement: 3,
+		search: '',
 		beforeEditCache: '',
 		elements: [ 
 			{
-				text: "Сделать что-то",
+				title: "Сделать что-то",
 				editing: false,
 				completed: true,
 				id: 1,
 			},
 			{
-				text: "Сделать то-то",
+				title: "Сделать то-то",
 				editing: false,
 				completed: false,
 				id: 2,
 			},
 			{
-				text: "Сделать совсем другое",
+				title: "Сделать совсем другое",
+				editing: false,
+				completed: false,
+				id: 3,
+			},
+		],
+
+		filter: 'all',
+    },
+    actions: {},
+    mutations: {
+    	
+    },
+    getters: {},  
+    modules: {}
+})
+
+
+var app = new Vue ({
+	el: '#app',
+	store,
+	data: {
+		newElement: '',
+		idForElement: 4,
+		search: '',
+		beforeEditCache: '',
+		elements: [ 
+			{
+				title: "сделать что-то",
+				editing: false,
+				completed: true,
+				id: 1,
+			},
+			{
+				title: "выгулять кого-то",
+				editing: false,
+				completed: false,
+				id: 2,
+			},
+			{
+				title: "купить то-то",
 				editing: false,
 				completed: false,
 				id: 3,
@@ -30,7 +72,7 @@ var app = new Vue ({
 	},
 	directives: {
 	  focus: {
-	    inserted: function (el) {
+	    inserted (el) {
 	      el.focus()
 	    }
 	  }
@@ -38,44 +80,44 @@ var app = new Vue ({
 	methods: {
 		addElement () {
 
-				if (this.newElement.trim() == 0) {
-					return
-				}
-			
-				this.elements.push ({
-					editing: false,
-					completed: false,
-					id: this.idForElement,
-					text: this.newElement,
-				})
+			if (this.newElement.trim() == 0) {
+				return
+			}
+		
+			this.elements.push ({
+				editing: false,
+				completed: false,
+				id: this.idForElement,
+				title: this.newElement,
+			})
 
-				this.newElement = ''
-				this.idForElement++
-			},
+			this.newElement = ''
+			this.idForElement++
+		},
 
 
 		
 
-		delElement: function (index) {
-			this.elements.splice(index, 1);
+		delElement (index) {
+			this.elements.splice(index, 1)
 		},
 
-		editEl: function (element) {
-			this.beforeEditCache = element.text
+		editEl (element) {
+			this.beforeEditCache = element.title
 			element.editing = true
 		},
 
-		doneEditEl: function (element) {
-			if (element.text.trim() == 0) {
-				element.text = this.beforeEditCache
+		doneEditEl (element) {
+			if (element.title.trim() == 0) {
+				element.title = this.beforeEditCache
 			}
 			element.editing = false
 		},
 
-		cancelEditEl: function (element) {
-			element.text = this.beforeEditCache
+		cancelEditEl (element) {
+			element.title = this.beforeEditCache
 			element.editing = false
-		}
+		},
 
 		
 	},
@@ -83,12 +125,29 @@ var app = new Vue ({
 		elementsFiltered () {
 			if (this.filter == 'all') {
 				return this.elements
-			}
-			else if (this.filter == 'completed') {
+			} else if (this.filter == 'completed') {
 				return this.elements.filter(element => element.completed)
 			} else if (this.filter == 'active') {
 				return this.elements.filter(element => !element.completed)
+			} else if (this.filter == 'search') {
+				return this.elements.filter((element) => {
+					return element.title.match(this.search)
+				})
 			}
+
+		},
+
+		elementsSearch () {
+			if (element.charAt(0).toLowerCase()) {
+				return this.element.charAt(0).toUpperCase()
+			} 
+		},
+
+	},
+
+	filters: {
+		capitalize (element) {
+		    return element.charAt(0).toUpperCase() + element.slice(1)
 		}
 	}
 })
